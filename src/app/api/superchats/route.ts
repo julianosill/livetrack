46d133150647cfb9654093327a10a1b973ff0892
Juliano Sill <sill.juliano@gmail.com>
2025-01-11 10:59:29 -0300

    chore: create superchats api route for testing

--- src/app/api/superchats/route.ts
+++ src/app/api/superchats/route.ts
@@ -1,49 +1,45 @@
-'use server'
-
 import { google } from 'googleapis'
+import { NextResponse } from 'next/server'
 
 import { superChatAdapter } from '@/adapters'
 import { getSession } from '@/auth'
+import { ROUTES } from '@/constants'
 import { wait } from '@/helpers'
 import { googleOAuth2Client } from '@/lib/google'
 import type { SuperChatType } from '@/types'
 
-interface FetchSuperChatsResult {
-  success: boolean
-  errorMessage?: string
-  superchats?: SuperChatType[]
-}
-
-export async function fetchSuperchats(): Promise<FetchSuperChatsResult> {
+export async function GET() {
   const session = await getSession()
-  if (!session) return { success: false }
+  if (!session) return NextResponse.redirect(ROUTES.api.auth.signOut)
 
   googleOAuth2Client.setCredentials(session.tokens)
   const youtube = google.youtube({ version: 'v3', auth: googleOAuth2Client })
 
   try {
+    let tries = 0
+    let firstResult
     let superchats: SuperChatType[] = []
-    let nextPageToken: string | undefined
+    let nextPageToken: string = ''
 
     do {
       const result = await youtube.superChatEvents.list({ part: ['snippet'], pageToken: nextPageToken })
 
-      if (result.status !== 200) {
-        return { success: false, errorMessage: 'Não foi possível obter os SuperChats do seu canal.' }
-      }
+      if (result.status !== 200) return NextResponse.json({ error: 'Requisição falhou' })
+
+      if (tries <= 0) firstResult = result.data
+      tries++
 
       const items = result.data.items ?? []
       if (items.length <= 0) break
 
       superchats = superchats.concat(items.map(superChatAdapter))
-      nextPageToken = result.data.nextPageToken ?? undefined
+      nextPageToken = result.data.nextPageToken ?? ''
 
-      await wait(500)
+      await wait(250)
     } while (nextPageToken)
 
-    return { success: true, superchats }
+    return NextResponse.json({ firstResult, superchats })
   } catch (error) {
-    console.error('Error in fetchSuperchats:', error)
-    return { success: false, errorMessage: 'Não foi possível obter os SuperChats do seu canal.' }
+    return NextResponse.json(error)
   }
 }
